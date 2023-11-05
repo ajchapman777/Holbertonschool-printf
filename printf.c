e976c0d3aad3afc2c865eeb2cad2984af7778a24
Ashley Chapman <ajchapman777@gmail.com>
2023-11-04 19:49:53 -0700

    printf

---
+++ printf.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "putchar.h"
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    
+    int count = 0;  // To keep track of the number of characters printed
+    
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++; // Move past the '%'
+            
+            if (*format == 'c') {
+                char character = va_arg(args, int);
+                _putchar(character);
+                count++;
+            } else if (*format == 's') {
+                char *str = va_arg(args, char *);
+                while (*str) {
+                    _putchar(*str);
+                    str++;
+                    count++;
+                }
+            } else if (*format == '%') {
+                _putchar('%');
+                count++;
+            }
+        } else {
+            _putchar(*format);
+            count++;
+        }
+        
+        format++; // Move to the next character in the format string
+    }
+    
+    va_end(args);
+    return count;
+}
